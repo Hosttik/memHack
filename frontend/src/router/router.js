a006362c8c5907ba92efcf5ab240ce85f74c99e1
s.svistunov <W123w007Wv4>
2020-04-04 13:10:41 +0300

    Loader

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "src/components/Home/Home";
+import Load from "src/components/Load/Load";
 import Resources from "src/components/Resources/Resources";
 
 Vue.use(Router);
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "Главная",
-      component: Home
+      name: "Загрузить фото",
+      component: Load
     },
     {
       path: "/resources",
