61b266a0590ef7b984996b7eb3e1a21f50f1000a
kirill <k.malkov@life-stream.tv>
2020-04-04 13:08:06 +0300

    header fixes + webpack fix

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "src/components/Home/Home";
-import Resources from "src/components/Resources/Resources";
+import Account from "src/components/Account/Account";
 
 Vue.use(Router);
 
@@ -14,9 +14,9 @@ export default new Router({
       component: Home
     },
     {
-      path: "/resources",
-      name: "Ресурсы",
-      component: Resources
+      path: "/account",
+      name: "Личный кабинет",
+      component: Account
     }
   ]
 });
