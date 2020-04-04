7f79d5660d75daf653ff36733c469111ac7f9c6e
kirill <k.malkov@life-stream.tv>
2020-04-04 14:57:49 +0300

    filter fix

--- frontend/src/components/Filters/Filters.vue
+++ frontend/src/components/Filters/Filters.vue
@@ -6,8 +6,8 @@
         <v-flex xs9>
             <v-card class="full-height">
                 <div class="photo-wrap">
-                    <div>sdfsdf</div>
-                    <div>dsfsdgsd</div>
+                    <div class="origin-photo"><img class="img-photo"/></div>
+                    <div class="future-photo"><img class="img-photo"/></div>
                 </div>
                 <div class="next-button-wrap">
                     <v-btn outline
@@ -36,15 +36,36 @@
     .full-height {
         height: 100%;
     }
+
     .photo-wrap {
         width: 80%;
         display: inline-flex;
+        height: 100%;
     }
+
     .next-button-wrap {
         display: inline-flex;
         align-items: center;
         justify-content: center;
         height: 100%;
+    }
+
+    .origin-photo {
+        width: 50%;
+        height: 100%;
+        float: left;
+    }
+
+    .future-photo {
+        width: 50%;
+        height: 100%;
+        float: right;
+    }
+    .img-photo {
+        background: grey;
+        width: 80%;
+        height: 90%;
+        vertical-align: middle;
     }
 
 </style>
