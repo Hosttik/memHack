3bab16844580ee43f07bbea1c71ec88ba861833b
kirill <k.malkov@life-stream.tv>
2020-04-04 13:57:21 +0300

    add filters comp and show test request

---
+++ frontend/src/components/Filters/Filters.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-layout row>
+        <v-flex class="full-height" xs3>
+            <v-card class="full-height">sdfdfgdfg</v-card>
+        </v-flex>
+        <v-flex xs9>
+            <v-card>sdfdfgdfg</v-card>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+  export default {
+    name: 'Filters',
+    data() {
+      return {
+        filtersList: ['']
+      };
+    },
+  }
+</script>
+
+<style scoped>
+    .full-height {
+        min-height: 100%;
+    }
+
+</style>
--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,22 +1,24 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "src/components/Home/Home";
-import Account from "src/components/Account/Account";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from 'src/components/Home/Home';
+import Account from 'src/components/Account/Account';
+import Filters from 'src/components/Filters/Filters';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
-  routes: [
-    {
-      path: "/",
-      name: "Главная",
-      component: Home
-    },
-    {
-      path: "/account",
-      name: "Личный кабинет",
-      component: Account
-    }
-  ]
+  mode: 'history',
+  routes: [{
+    path: '/',
+    name: 'Главная',
+    component: Home
+  }, {
+    path: '/account',
+    name: 'Личный кабинет',
+    component: Account
+  }, {
+    path: '/filters',
+    name: 'Фильтры',
+    component: Filters
+  }]
 });
